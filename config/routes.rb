ec6bac434de3767913fcbdc6ed4107855b357f14
Hadrien VAISON <hadrienvaison@gmail.com>
2021-05-24 18:01:33 +0200

    route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :rooms, only: [:show, :create] do
+    resources :runnings, only: :create
     resources :races, only: [:show, :create] do
-      resources :runnings, only: [:create, :index]
-      resources :horses, only: [:create, :index]
+      resources :runnings, only: :index
+      resources :horses, only: :index
       resources :bets, only: [:new, :edit, :create, :update]
     end
   end
